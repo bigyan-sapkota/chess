d168e95b43f7bae5a0715d64fb4bfe72751783de
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2024-09-12 20:21:45 +0545

    set up backend

--- app.js
+++ app.js
@@ -23,6 +23,47 @@ app.get("/", (req, res) => {
 
 io.on("connection", function (uniquesocket) {
   console.log("connected");
+
+  if (!players.white) {
+    players.white = uniquesocket.id;
+    uniquesocket.emit("playerRole", "w");
+  } else if (!players.black) {
+    players.black = uniquesocket.id;
+    uniquesocket.emit("playerRole", "b");
+  } else {
+    uniquesocket.emit("spectatorRole");
+  }
+
+  uniquesocket.on("disconnect", function () {
+    if (uniquesocket.id === players.white) {
+      delete players.white;
+    } else if (uniquesocket.id === players.black) {
+      delete players.black;
+    }
+  });
+
+  // if piece move check if the move is valid move and turn is of valid player
+  uniquesocket.on("move", (move) => {
+    try {
+      // only player with turn plays move
+      if (chess.turn() === "w" && uniquesocket.id !== players.white) return;
+      if (chess.turn() === "b" && uniquesocket.id !== players.black) return;
+
+      const result = chess.move(move);
+
+      if (result) {
+        currentPlayer = chess.turn();
+        io.emit("move", move);
+        io.emit("boardState", chess.fen());
+      } else {
+        console.log("Invalid move : ", move);
+        uniquesocket.emit("Invalid Move : ", move);
+      }
+    } catch (error) {
+      console.log(error);
+      uniquesocket.emit("Invalid move : ", movae);
+    }
+  });
 });
 
 server.listen(3000 || 8000, function () {
